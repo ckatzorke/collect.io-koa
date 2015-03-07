a8b527acdd780d20091a6803cb38ef12ea9f1034
Christian Katzorke <c.katzorke@web.de>
2015-03-07 22:45:54 +0100

    solve promise issue. using indexdb for storage

--- src/client/js/collectio-storage.js
+++ src/client/js/collectio-storage.js
@@ -40,7 +40,11 @@ var collectioStorage = (function () {
         });
         return dbDeferred;
     };
-
+    
+    /**
+     * Adds or updates a game entry.
+     * @param game the object to store, containes the id, the gamebomb details, and collectio metadata
+     */
     var addGame = function (game) {
         // Store values in the newly created objectStore.
         _getDB().then(function (db) {
@@ -51,10 +55,31 @@ var collectioStorage = (function () {
             };
         });
     };
+    /**
+     * Looks up the game with given id
+     * @param id the id to look for
+     * @return the stored object, if found, undefined if not found
+     */
+    var getGameById = function (id) {
+        var gameDeferred = new Promise(function (resolve, reject) {
+            _getDB().then(function (db) {
+                var transaction = db.transaction(['games']);
+                var objectStore = transaction.objectStore('games');
+                var request = objectStore.get(id);
+                request.onerror = function (event) {
+                    // Handle errors!
+                };
+                request.onsuccess = function (event) {
+                    resolve(request.result);
+                };
+            });
+        });
+        return gameDeferred;
+    };
 
     var getAll = function () {
-        _getDB().then(function (db) {
-            var gamesDeferred = new Promise(function (resolve, reject) {
+        var gamesDeferred = new Promise(function (resolve, reject) {
+            _getDB().then(function (db) {
                 var games = [];
                 var objectStore = db.transaction('games').objectStore('games');
                 objectStore.openCursor().onsuccess = function (event) {
@@ -68,15 +93,12 @@ var collectioStorage = (function () {
                     }
                 };
             });
-            return gamesDeferred;
-        }).then(function (games) {
-            return new Promise(function (resolve) {
-                resolve(games);
-            });
         });
-    }
+        return gamesDeferred;
+    };
     return {
         'add': addGame,
+        'getGameById': getGameById,
         'getAll': getAll
     };
 }());
--- src/client/js/collectio.js
+++ src/client/js/collectio.js
@@ -1,6 +1,6 @@
 var collectio = (function ($) {
     'use strict';
-    
+
     var collectionView = (function () {
         var state = {
             sort: {
@@ -14,7 +14,7 @@ var collectio = (function ($) {
             panel.slideToggle('slow');
             return false;
         };
-        
+
         //public API
         return {
             'state': state,
@@ -29,10 +29,87 @@ var collectio = (function ($) {
 }(jQuery));
 
 
+
+
+var quicksearch = (function ($) {
+    'use strict';
+    var tSource = $('#searchresults-template').html();
+    var template = Handlebars.compile(tSource);
+    var search = function () {
+        var ret = new Promise(function (resolve, reject) {
+            var value = $('#quickSearchInput').val();
+            $.getJSON('/gb/search?q=' + value, function (data) {
+                console.log(data);
+                var resultHtml = data.results.length === 0 ? 'No results...' : template(data);
+                console.log(resultHtml);
+                $('#searchresult').html(resultHtml);
+                resolve();
+            });
+        });
+        return ret;
+    };
+    return {
+        search: search
+    };
+})(jQuery);
+
+var collection = (function ($) {
+    'use strict';
+    var tSource = $('#collection-entry-template').html();
+    var template = Handlebars.compile(tSource);
+    var add = function (id, name, deck, thumbUrl) {
+        var entry = {
+            'id': id,
+            'name': name,
+            'deck': deck,
+            'thumb': thumbUrl
+        };
+        addGame(entry);
+        $('#addModal').modal('hide');
+    };
+    var addGame = function (game) {
+        var now = new Date();
+        var store = {
+            'id': game.id,
+            'gamedetails': game,
+            'added': now,
+            'updated': now
+        };
+        collectioStorage.add(store);
+        console.log('adding', store);
+        renderEntry(store);
+    };
+    var renderEntry = function (entry) {
+        var entryHtml = template(entry);
+        console.log('Entry HTML', entryHtml);
+        $('#top-button-bar').after(entryHtml);
+    };
+    return {
+        add: add,
+        addGame: addGame,
+        renderEntry: renderEntry
+    };
+}(jQuery));
+
+
+
 //init
 $(document).ready(function () {
     'use strict';
     $('#category-slide-panel-opener').on('click', function () {
         collectio.collectionView.toggleCategoryPanel();
+    });
+    $('#quickSearchBtn').click(function () {
+        quicksearch.search();
+    });
+    $('#quickSearchInput').keypress(function (e) {
+        if (e.which === 13) { //Enter key pressed
+            $('#quickSearchBtn').click(); //Trigger search button click event
+        }
+    });
+    collectioStorage.getAll().then(function (games) {
+        games.map(function (game) {
+            collection.renderEntry(game);
+        });
     });
 });
